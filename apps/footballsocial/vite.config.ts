d1f1ae2fc0600f2a23f4bc78d68a9f9591151264
Sam Ternent <sam.ternent@gmail.com>
2022-12-05 23:31:09 +0000

    resolve env path

--- apps/footballsocial/vite.config.ts
+++ apps/footballsocial/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
@@ -7,7 +8,7 @@ const domain = "footballsocial.localhost";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: "../../",
+  envDir: resolve("../../"),
   server: {
     proxy: {
       "/api": {
