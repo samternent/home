ba9ad719ffe4934192af0b4a47f64aa82e3887ec
Sam Ternent <sam.ternent@gmail.com>
2023-03-20 21:38:45 +0000

    fix build

--- apps/gzip/vite.config.ts
+++ apps/gzip/vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import Markdown from "vite-plugin-md";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +25,7 @@ export default defineConfig({
         navigateFallbackDenylist: [/^\/api/],
       },
     }),
+    Markdown(),
   ],
   resolve: {
     alias: {
