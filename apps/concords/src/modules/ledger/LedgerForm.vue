3ce61dbe0acf0e8beb659f98fc20a09a7888111b
Sam Ternent <sam.ternent@gmail.com>
2023-03-29 12:59:44 +0100

    lockfile

--- apps/concords/src/modules/ledger/LedgerForm.vue
+++ apps/concords/src/modules/ledger/LedgerForm.vue
@@ -240,7 +240,7 @@ function getValue(type: string, name: string, id: string) {
         @click="addListItem"
         class="bg-green-500 hover:bg-green-600 py-2 px-6 block w-full rounded text-sm"
       >
-        {{ item ? "Add" : "Update" }}
+        {{ Object.keys(item).length ? "Update" : "Add" }}
       </button>
     </td>
   </tr>
