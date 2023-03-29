d6c6662d7fbe647a93c42170b0b1f71b0a056257
Sam Ternent <sam.ternent@gmail.com>
2023-03-30 00:14:20 +0100

    fix casing

--- apps/concords/src/Setup.vue
+++ apps/concords/src/Setup.vue
@@ -2,7 +2,7 @@
 import { provideCurrentUser } from "@/api/useCurrentUser";
 import { Identity } from "@/modules/identity";
 import { Encryption } from "@/modules/encryption";
-import { Ledger } from "@/modules/Ledger";
+import { Ledger } from "@/modules/ledger";
 import { Solid } from "@/modules/solid";
 
 const { ready } = provideCurrentUser();
