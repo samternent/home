27987920b228ca6c27beb0ed6becc0ebc2c898b1
Sam Ternent <sam.ternent@gmail.com>
2023-03-30 10:55:41 +0100

    no current user

--- apps/concords/src/Setup.vue
+++ apps/concords/src/Setup.vue
@@ -1,11 +1,8 @@
 <script setup>
-import { provideCurrentUser } from "@/api/useCurrentUser";
 import { Identity } from "@/modules/identity";
 import { Encryption } from "@/modules/encryption";
 import { Ledger } from "@/modules/ledger";
 import { Solid } from "@/modules/solid";
-
-const { ready } = provideCurrentUser();
 </script>
 
 <template>
