c080d980097e96eea52fa60695c5e2f01dfc0c3d
Sam Ternent <sam.ternent@gmail.com>
2023-06-02 16:12:52 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 # #!/bin/bash
 # export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# # TODO: Rename repo in git and reeflect here
-# cd footballsocial
+# TODO: Rename repo in git and reeflect here
+cd footballsocial
 
-# # pull for latest server code
-# git pull origin main
-# pnpm i
+# pull for latest server code
+git pull origin main
+pnpm i
 
-# echo "start node servers"
-# # start/restart node server
-# NODE_ENV=production pm2 startOrReload ecosystem.config.cjs
+echo "start node servers"
+# start/restart node server
+NODE_ENV=production pm2 startOrReload ecosystem.config.cjs
 
 # cd ~
 
