2874c9800a6957ee804fa6909d69c44f43db2356
Sam Ternent <sam.ternent@gmail.com>
2023-08-04 00:23:45 +0100

    h

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ sudo rm -rf  /etc/nginx/sites-available/*
 
 # api.ternent.dev
 sudo cp -r apps/ternent/nginx.conf.d /etc/nginx/sites-available/ternent.dev
-sudo ln -s /etc/nginx/sites-available/ternent.dev ~/etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/ternent.dev /etc/nginx/sites-enabled/
 
 # api.footballsocial.app
 sudo cp -r apps/footballsocial/nginx.conf.d /etc/nginx/sites-available/footballsocial.app
