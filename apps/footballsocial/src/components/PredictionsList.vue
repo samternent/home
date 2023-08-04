8f5b737b45500166adc2a87507c75e96201ef50e
Sam Ternent <sam.ternent@gmail.com>
2023-08-04 23:47:39 +0100

    disable in play and finished predictions

--- apps/footballsocial/src/components/PredictionsList.vue
+++ apps/footballsocial/src/components/PredictionsList.vue
@@ -106,7 +106,7 @@ const isDirty = computed(
               :fixture="fixture"
               :size="size"
               :disabled="
-                meta.played > 1 ||
+                ['IN_PLAY', 'FINISHED'].includes(fixture.status) ||
                 (username && profile?.username !== username) ||
                 false
               "
