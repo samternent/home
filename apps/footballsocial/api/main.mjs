e5982a5b8a65c2c12d24856f7031b95791630581
Sam Ternent <sam.ternent@gmail.com>
2023-08-04 01:34:28 +0100

    cors

--- apps/footballsocial/api/main.mjs
+++ apps/footballsocial/api/main.mjs
@@ -1,11 +1,18 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import bodyParser from "body-parser";
+import cors from "cors";
+
+var corsOptions = {
+  origin: "https://footballsocial.app",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // App
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 // Set port
 const port = "4002";
