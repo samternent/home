96ae2269be0d972a4f4cf1af9141b8b5a389dbe3
Sam Ternent <sam.ternent@gmail.com>
2023-08-05 16:47:32 +0100

    update concords app name

--- build/publish.sh
+++ build/publish.sh
@@ -1,21 +1,22 @@
-
 #!/bin/bash
 set -e # exit when error
 
-FS_PACKAGE_NAME=`node -p "require('./apps/footballsocial/package.json').name"`
-FS_PACKAGE_VERSION=`node -p "require('./apps/footballsocial/package.json').version"`
+FS_PACKAGE_NAME=$(node -p "require('./apps/footballsocial/package.json').name")
+FS_PACKAGE_VERSION=$(node -p "require('./apps/footballsocial/package.json').version")
 
-CL_PACKAGE_NAME=`node -p "require('./packages/ledger/package.json').name"`
-CL_PACKAGE_VERSION=`node -p "require('./packages/ledger/package.json').version"`
+CA_PACKAGE_NAME=$(node -p "require('./apps/concords/package.json').name")
+CA_PACKAGE_VERSION=$(node -p "require('./apps/concords/package.json').version")
 
-packageArray=("${FS_PACKAGE_NAME}-${FS_PACKAGE_VERSION}" "${CL_PACKAGE_NAME}-${CL_PACKAGE_VERSION}")
+CL_PACKAGE_NAME=$(node -p "require('./packages/ledger/package.json').name")
+CL_PACKAGE_VERSION=$(node -p "require('./packages/ledger/package.json').version")
+
+packageArray=("${FS_PACKAGE_NAME}-${FS_PACKAGE_VERSION}" "${CL_PACKAGE_NAME}-${CL_PACKAGE_VERSION}" "${CA_PACKAGE_NAME}-${CA_PACKAGE_VERSION}")
 
 # Accessing the passed branch name as a command line argument
 branch_name="${GITHUB_REF#refs/heads/}"
 
-for str in ${packageArray[@]};
-do
-   if [ $(git tag -l "${str}") ]; then
+for str in ${packageArray[@]}; do
+  if [ $(git tag -l "${str}") ]; then
     echo "> release ${str} already exists."
     echo "> Aborting..."
     # exit 1
@@ -31,7 +32,7 @@ do
     # get the merged commit SHA so we can tag this release precisely and avoid including any subsequest merges
     releaseMergeCommitSHA=$(gh pr list -B "$branch_name" -s merged -H changeset-release/"$branch_name" --json mergeCommit --jq '.[].mergeCommit.oid' -L 1)
 
-    releaseReponse=$(gh release create "${str}" -t "${str}" -n "${str}" -n "$releaseNotes" --target "${releaseMergeCommitSHA}" )
+    releaseReponse=$(gh release create "${str}" -t "${str}" -n "${str}" -n "$releaseNotes" --target "${releaseMergeCommitSHA}")
 
   fi
 done
