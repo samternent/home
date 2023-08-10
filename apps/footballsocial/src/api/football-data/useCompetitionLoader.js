88966bf1f0efedfc5e8db1799fc2d5ccdb50b2d8
Sam Ternent <sam.ternent@gmail.com>
2023-08-10 23:43:27 +0100

    fix debug

--- apps/footballsocial/src/api/football-data/useCompetitionLoader.js
+++ apps/footballsocial/src/api/football-data/useCompetitionLoader.js
@@ -1,37 +1,33 @@
-import { inject, provide, shallowRef, watch, computed } from 'vue';
-import api from '../../utils/api';
+import { inject, provide, shallowRef, watch, computed } from "vue";
+import api from "../../utils/api";
 
-const useCompetitionLoaderSymbol = Symbol('useCompetitionLoader');
+const useCompetitionLoaderSymbol = Symbol("useCompetitionLoader");
 
 export function provideCompetitionLoader(competitionCode) {
-	const items = shallowRef();
-	const loading = shallowRef(false);
-	const loaded = shallowRef(false);
+  const items = shallowRef();
+  const loading = shallowRef(false);
+  const loaded = shallowRef(false);
 
-	async function fetchCompetitions(_competitionCode = null) {
-		if (!_competitionCode) return;
-		loading.value = true;
-		try {
-			const { data } = await api.get(
-				`football-data/competitions/${_competitionCode}`
-			);
-		} catch (e) {
-			alert(e)
-		}
-		items.value = data;
-		loaded.value = true;
-		loading.value = false;
-	}
+  async function fetchCompetitions(_competitionCode = null) {
+    if (!_competitionCode) return;
+    loading.value = true;
+    const { data } = await api.get(
+      `football-data/competitions/${_competitionCode}`
+    );
+    items.value = data;
+    loaded.value = true;
+    loading.value = false;
+  }
 
-	const hasItems = computed(() => !loading.value && loaded.value);
+  const hasItems = computed(() => !loading.value && loaded.value);
 
-	watch(competitionCode, fetchCompetitions, { immediate: true });
+  watch(competitionCode, fetchCompetitions, { immediate: true });
 
-	const competitionObject = { items, loading, loaded, hasItems };
-	provide(useCompetitionLoaderSymbol, competitionObject);
-	return competitionObject;
+  const competitionObject = { items, loading, loaded, hasItems };
+  provide(useCompetitionLoaderSymbol, competitionObject);
+  return competitionObject;
 }
 
 export function useCompetitionLoader() {
-	return inject(useCompetitionLoaderSymbol);
+  return inject(useCompetitionLoaderSymbol);
 }
