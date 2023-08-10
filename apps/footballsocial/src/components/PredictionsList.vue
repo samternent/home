c27b9eed0953b6f6854f6c800793346f4918baac
Sam Ternent <sam.ternent@gmail.com>
2023-08-10 12:22:54 +0100

    disable if not logged in

--- apps/footballsocial/src/components/PredictionsList.vue
+++ apps/footballsocial/src/components/PredictionsList.vue
@@ -1,11 +1,6 @@
 <script setup>
-import {
-  toRefs,
-  computed,
-  shallowRef,
-  unref,
-} from "vue";
-import { watchThrottled } from '@vueuse/core'
+import { toRefs, computed, shallowRef, unref } from "vue";
+import { watchThrottled } from "@vueuse/core";
 import useFixturesLoader from "../api/football-data/useCompetitionFixturesLoader";
 import {
   addPrediction,
@@ -70,7 +65,7 @@ async function loadPredictions() {
   data.predictions?.forEach((prediction) => {
     lockedPredictions.value = [
       ...lockedPredictions.value,
-      prediction.fixtureId
+      prediction.fixtureId,
     ];
 
     predictions.value = {
@@ -110,18 +105,17 @@ const isDirty = computed(
   () => serverPredictions.value !== JSON.stringify(predictions.value)
 );
 
+watchThrottled([gameweek, competitionCode], loadPredictions, {
+  throttle: 500,
+  immediate: true,
+});
 
-watchThrottled(
-  [gameweek, competitionCode],
-  loadPredictions,
-  { throttle: 500, immediate: true },
-)
-
-const predictionsList = computed(() =>
-  fixtures.value?.map((fixture) => ({
-    ...fixture,
-    prediction: predictions.value[fixture.id],
-  })) || []
+const predictionsList = computed(
+  () =>
+    fixtures.value?.map((fixture) => ({
+      ...fixture,
+      prediction: predictions.value[fixture.id],
+    })) || []
 );
 
 const hasPredictions = computed(() => {
@@ -130,57 +124,66 @@ const hasPredictions = computed(() => {
 });
 </script>
 <template>
-    <div class="w-full flex flex-col mx-auto" v-if="predictionsLoaded">
-      <div class="text-center my-0 flex flex-col">
-        <span
-          class="text-xl p-2 font-thin my-4 mx-4 bg-indigo-500 rounded-full bg-opacity-30"
-          v-if="hasPredictions"
-          >{{ username ? `${username}s` : "Your" }} predictions are in!</span
-        >
-        <span
-          class="text-lg p-2 font-thin my-4 text-indigo-300 mx-4 bg-indigo-800 rounded-full bg-opacity-30"
-          v-else
-          >predictions not yet made.</span
-        >
-      </div>
-      <CountdownTimer :gameweek="gameweek" :kickOff="predictionsList[0]?.utcDate" />
-      <div v-for="(fixture, i) in predictionsList" :key="fixture.id" class="mx-auto w-full flex-1">
-        <Predictor
-          :fixture="fixture"
-          :size="size"
-          :disabled="
-            new Date(fixture.utcDate).getTime() < new Date().getTime() ||
-            ['IN_PLAY', 'FINISHED'].includes(fixture.status) ||
-            (username && profile?.username !== username) ||
-            false
-          "
-          :showDate="fixture.utcDate !== fixtures[i - 1]?.utcDate"
-          :prediction="fixture.prediction"
-          @update:prediction="
-            (prediction) => updatePrediction(prediction, fixture)
-          "
-          @click="(fixture) => $emit('selected', fixture)"
-        />
-      </div>
-      <div class="text-center mt-8 flex flex-col items-end">
-        <button
-          :disabled="!isDirty"
-          v-if="profile && (!username || profile.username === username)"
-          @click="savePredictions"
-          class="bg-green-700 font-thin w-64 text-xl uppercase disabled:opacity-20 rounded p-2 my-2"
-        >
-          Save predictions
-        </button>
-      </div>
+  <div class="w-full flex flex-col mx-auto" v-if="predictionsLoaded">
+    <div class="text-center my-0 flex flex-col">
+      <span
+        class="text-xl p-2 font-thin my-4 mx-4 bg-indigo-500 rounded-full bg-opacity-30"
+        v-if="hasPredictions"
+        >{{ username ? `${username}s` : "Your" }} predictions are in!</span
+      >
+      <span
+        class="text-lg p-2 font-thin my-4 text-indigo-300 mx-4 bg-indigo-800 rounded-full bg-opacity-30"
+        v-else
+        >predictions not yet made.</span
+      >
     </div>
-
-    <div v-else class="flex flex-col h-screen w-full pr-4 pt-1">
-      <div
-        v-for="i in 10"
-        :key="i"
-        class="bg-[#1e1e1e] animate-pulse m-2 my-4 rounded flex-1 h-20 w-full"
+    <CountdownTimer
+      :gameweek="gameweek"
+      :kickOff="predictionsList[0]?.utcDate"
+    />
+    <div
+      v-for="(fixture, i) in predictionsList"
+      :key="fixture.id"
+      class="mx-auto w-full flex-1"
+    >
+      <Predictor
+        :fixture="fixture"
+        :size="size"
+        :disabled="
+          new Date(fixture.utcDate).getTime() < new Date().getTime() ||
+          ['IN_PLAY', 'FINISHED'].includes(fixture.status) ||
+          !username ||
+          !profile?.username ||
+          (username && profile?.username !== username) ||
+          false
+        "
+        :showDate="fixture.utcDate !== fixtures[i - 1]?.utcDate"
+        :prediction="fixture.prediction"
+        @update:prediction="
+          (prediction) => updatePrediction(prediction, fixture)
+        "
+        @click="(fixture) => $emit('selected', fixture)"
       />
     </div>
+    <div class="text-center mt-8 flex flex-col items-end">
+      <button
+        :disabled="!isDirty"
+        v-if="profile && (!username || profile.username === username)"
+        @click="savePredictions"
+        class="bg-green-700 font-thin w-64 text-xl uppercase disabled:opacity-20 rounded p-2 my-2"
+      >
+        Save predictions
+      </button>
+    </div>
+  </div>
+
+  <div v-else class="flex flex-col h-screen w-full pr-4 pt-1">
+    <div
+      v-for="i in 10"
+      :key="i"
+      class="bg-[#1e1e1e] animate-pulse m-2 my-4 rounded flex-1 h-20 w-full"
+    />
+  </div>
 </template>
 <style scoped>
 .v-enter-active,
