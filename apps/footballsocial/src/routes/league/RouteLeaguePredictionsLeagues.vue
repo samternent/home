cca58527b30059675fe2624fc1dbd765c6c1750d
Sam Ternent <sam.ternent@gmail.com>
2023-08-16 02:01:10 +0100

    flex

--- apps/footballsocial/src/routes/league/RouteLeaguePredictionsLeagues.vue
+++ apps/footballsocial/src/routes/league/RouteLeaguePredictionsLeagues.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     default: null,
   },
 });
-const { items: competition} = useCompetitionLoader();
+const { items: competition } = useCompetitionLoader();
 </script>
 <template>
   <!-- <div class="flex items-center flex-col justify-center w-full">
@@ -24,20 +24,22 @@ const { items: competition} = useCompetitionLoader();
       all predictions are stored and will be added to the table upon release.
     </p>
   </div> -->
-  <PredictionsResults
-    v-if="competition"
-    :competitionCode="competition?.code"
-  />
-  <div class="p-4 mt-6 text-zinc-200">
-    <h3 class="text-xl font-light text-white">Rules</h3>
-    <ul class="text-sm font-light my-2">
-      <li>* 1 point for a correct home score</li>
-      <li>* 1 point for a correct away score</li>
-      <li>* 2 points for a correct result (W/L/D)</li>
-      <li>* 3 points for a correct score</li>
-    </ul>
-    <p class="text-lg font-light my-8 text-white">
-      Rules and point system are subject to change.
-    </p>
+  <div class="w-full">
+    <PredictionsResults
+      v-if="competition"
+      :competitionCode="competition?.code"
+    />
+    <div class="p-4 mt-6 text-zinc-200">
+      <h3 class="text-xl font-light text-white">Rules</h3>
+      <ul class="text-sm font-light my-2">
+        <li>* 1 point for a correct home score</li>
+        <li>* 1 point for a correct away score</li>
+        <li>* 2 points for a correct result (W/L/D)</li>
+        <li>* 3 points for a correct score</li>
+      </ul>
+      <p class="text-lg font-light my-8 text-white">
+        Rules and point system are subject to change.
+      </p>
+    </div>
   </div>
 </template>
