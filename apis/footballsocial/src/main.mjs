06e10595e847c3159e9f22a59b5200e0adad638c
Sam Ternent <sam.ternent@gmail.com>
2023-09-05 00:46:28 +0100

    dockerfile fixes....maybe

--- apis/footballsocial/src/main.mjs
+++ apis/footballsocial/src/main.mjs
@@ -1,21 +1,19 @@
-console.log('hello, world!');
+import express from "express";
+import routes from "./routes/index.mjs";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-// import express from "express";
-// import routes from "./routes/index.mjs";
-// import bodyParser from "body-parser";
-// import cors from "cors";
+// App
+const app = express();
 
-// // App
-// const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-// app.use(bodyParser.json());
-// app.use(cors());
+// Set port
+const port = "4002";
+app.set("port", port);
 
-// // Set port
-// const port = "4002";
-// app.set("port", port);
+app.use("/", routes);
 
-// app.use("/", routes);
-
-// // Server
-// app.listen(port, () => console.log(`Server running on localhost:${port}`));
+// Server
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
