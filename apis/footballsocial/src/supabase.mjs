2375b560579827894da92888aa6c9c168728357f
Sam Ternent <sam.ternent@gmail.com>
2023-09-05 02:10:45 +0100

    env

--- apis/footballsocial/src/supabase.mjs
+++ apis/footballsocial/src/supabase.mjs
@@ -14,7 +14,7 @@ console.log(process.env.VITE_PUBLIC_SUPABASE_URL);
 
 const supabaseClient = createClient(
   process.env.VITE_PUBLIC_SUPABASE_URL || "",
-  process.env.VITE_PUBLIC_SUPABASE_ANON_KEY || "",
+  process.env.VITE_PUBLIC_SUPABASE_URL || "",
   supabaseOptions
 );
 
