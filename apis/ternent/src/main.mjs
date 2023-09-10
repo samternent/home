0318e4a78b249460d0b39e0eb807b0df15298924
Sam Ternent <sam.ternent@gmail.com>
2023-09-10 22:11:04 +0100

    k8s

--- apis/ternent/src/main.mjs
+++ apis/ternent/src/main.mjs
@@ -1,22 +1,32 @@
-import express from "express";
-import routes from "./routes/index.mjs";
-import bodyParser from "body-parser";
-import { join, dirname } from "path";
+import { readFileSync } from "fs";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import routes from "./routes/index.mjs";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // App
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors({ exposedHeaders: ["X-App-Version"] }));
 
 // Set port
-const port = "4005";
+const port = "4003";
 app.set("port", port);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.use("/assets", express.static(join(__dirname, "/assets")));
-app.use("/app", express.static(join(__dirname, "/app")));
+app.use(async function (req, res, next) {
+  const data = JSON.parse(
+    await readFileSync(join(__dirname, "../package.json"), "utf8")
+  );
+  res.setHeader("x-app-version", data.version);
+  next();
+});
 
 app.use("/", routes);
 
--- apps/gzip/README.md
+++ apps/gzip/README.md
@@ -1,1 +1,5 @@
-gzip.app
+# gzip.app
+
+- https://gzip.app
+
+[JSON compression in the browser, with gzip and the Compression Streams API.](https://dev.to/samternent/json-compression-in-the-browser-with-gzip-and-the-compression-streams-api-4135)
