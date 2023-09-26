12328b8196d8c52a4a3fd8845214930e0db81378
Sam Ternent <sam.ternent@gmail.com>
2023-09-26 23:02:24 +0100

    adds catchall for concords

--- apis/concords/src/routes/index.mjs
+++ apis/concords/src/routes/index.mjs
@@ -4,8 +4,11 @@ const router = express.Router();
 
 router.get("/hello/:name?", async function (req, res) {
   const { name } = req.params;
-
   return res.send(`Hello, ${name || "World"}!`);
+});
+
+router.get("/*", async function (req, res) {
+  return res.status(200).send("https://api.concords.app");
 });
 
 export default router;
