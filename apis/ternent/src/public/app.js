17effeac3073a9bef3353d475aa6d7b4e884c3cf
Sam Ternent <sam.ternent@gmail.com>
2023-09-28 21:13:01 +0100

    more styles

--- apis/ternent/src/public/app.js
+++ apis/ternent/src/public/app.js
@@ -12,11 +12,32 @@ socket.addEventListener("message", (event) => {
   console.log("message received: ", JSON.parse(event.data));
 });
 
+function throttle(callback, limit) {
+  var waiting = false; // Initially, we're not waiting
+  return function () {
+    // We return a throttled function
+    if (!waiting) {
+      // If we're not waiting
+      callback.apply(this, arguments); // Execute users function
+      waiting = true; // Prevent future invocations
+      setTimeout(function () {
+        // After a period of time
+        waiting = false; // And allow future invocations
+      }, limit);
+    }
+  };
+}
+
+function getVerticalScrollPercentage(el) {
+  const p = el.parentNode;
+  return (
+    ((el.scrollTop || p.scrollTop) / (p.scrollHeight - p.clientHeight)) * 100
+  );
+}
+
 // rainbow scroll
 window.addEventListener("scroll", (e) => {
-  let scrollTop = document.body.scrollTop
-    ? document.body.scrollTop
-    : document.documentElement.scrollTop;
-  let newPos = scrollTop + "px";
+  const pos = getVerticalScrollPercentage(document.body);
+  const newPos = `${Math.round(pos) + 5}%`;
   document.documentElement.style.setProperty("--scrollPos", newPos);
 });
