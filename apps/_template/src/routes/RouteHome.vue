eb1d7cdd9dba8096bc3e567a1620ffbfc511a614
Sam Ternent <sam.ternent@gmail.com>
2023-10-02 10:48:53 +0100

    remove unused code

--- apps/_template/src/routes/RouteHome.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <div class="w-full flex-1 flex flex-col">
-    <Teleport to="#TopFixedPanel"> </Teleport>
-    <Teleport to="#BottomPanelBanner"> </Teleport>
-    <Teleport to="#BottomPanelContent"> </Teleport>
-  </div>
-</template>
