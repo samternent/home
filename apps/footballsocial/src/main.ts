27a24d2d0e1406c0f6f1d6d11b63ada5a610e8be
Sam Ternent <sam.ternent@gmail.com>
2023-11-24 21:17:47 +0000

    remove sentry - perf not worth it

--- apps/footballsocial/src/main.ts
+++ apps/footballsocial/src/main.ts
@@ -2,32 +2,12 @@ import 'ternent-ui/tailwind';
 import 'ternent-ui/styles';
 
 import { createApp } from "vue";
-import * as Sentry from "@sentry/vue";
 
 import App from "./App.vue";
 import router from "./router";
 import registerSW from "./utils/registerSW";
 
 const app = createApp(App);
-
-Sentry.init({
-  app,
-  dsn: "https://78110d419c2a0afbf0b017a39c46a3eb@o4505977280593920.ingest.sentry.io/4505977283674112",
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/footballsocial\.app/],
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 0.1,
-  // Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
-
 
 app.use(router);
 registerSW();
