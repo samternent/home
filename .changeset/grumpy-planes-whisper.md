df81b8fd87ae1e906c9da463d1d2fe37610eb1c7
Sam Ternent <sam.ternent@gmail.com>
2023-12-21 00:35:18 +0000

    docs(changeset): Update UI nav

---
+++ .changeset/grumpy-planes-whisper.md
@@ -0,0 +1,5 @@
+---
+"ternent-ui": patch
+---
+
+Update UI nav
--- packages/ternent-ui/src/components/SNavBar.vue
+++ packages/ternent-ui/src/components/SNavBar.vue
@@ -16,15 +16,14 @@ defineProps({
       <div class="flex-1 items-center">
         <slot name="nav" v-if="smallerThanMd"> </slot>
         <slot name="start">
-          <div class="flex px-4"></div>
           <RouterLink
             to="/"
-            class="btn btn-ghost btn-sm text-base normal-case font-extrabold mr-4"
+            class="btn btn-ghost btn-sm text-base normal-case font-extrabold md:mx-4"
             >{{ title }}<span class="text-accent -mx-2">.</span></RouterLink
           >
         </slot>
       </div>
-      <div class="flex-none px-4">
+      <div class="flex-none md:px-4">
         <slot name="end" />
       </div>
     </div>
