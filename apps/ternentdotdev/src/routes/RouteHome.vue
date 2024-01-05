abeebeba47ca76c40a52dea7f2cdeab88f3008a6
Sam Ternent <sam.ternent@gmail.com>
2024-01-05 14:29:42 +0000

    docs(changeset): some copy

--- apps/ternentdotdev/src/routes/RouteHome.vue
+++ apps/ternentdotdev/src/routes/RouteHome.vue
@@ -3,10 +3,9 @@ import { SBrandHeader } from "ternent-ui/components";
 </script>
 <template>
   <div class="flex flex-col h-full flex-1 p-4">
-    <SBrandHeader class="!font-thin">Ternent Dot Dev Ltd</SBrandHeader>
+    <SBrandHeader class="!font-light">Ternent Dot Dev Ltd</SBrandHeader>
     <p class="my-6 text-2xl font-light">
-      A UK based web business, specializing in Frontend, Application and
-      Platform Engineering.
+      A UK based web business specializing in Frontend and Platform Engineering.
     </p>
   </div>
 </template>
