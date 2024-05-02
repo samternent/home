dc50f37790987c1dfad7ceffbd2d0ebfe484856c
samternent <sam.ternent@teamwork.com>
2024-05-02 10:14:43 +0100

    fix username not shoing on startup

--- apps/footballsocial/src/App.vue
+++ apps/footballsocial/src/App.vue
@@ -106,7 +106,7 @@ const theme = useLocalStorage(
 
       <Api>
         <div
-          v-if="user && !profile?.username"
+          v-if="user && !profile?.username && ready"
           class="fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
         >
           <div class="bg-base-100 px-4 rounded-lg shadow-lg w-96 text-center">
