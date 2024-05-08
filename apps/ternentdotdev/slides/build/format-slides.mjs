03fe3dc659a1d50603548d3a9d6d72ea13d2e6dc
samternent <sam.ternent@teamwork.com>
2024-05-08 11:42:21 +0100

    docs(changeset): trials

--- apps/ternentdotdev/slides/build/format-slides.mjs
+++ apps/ternentdotdev/slides/build/format-slides.mjs
@@ -1,11 +1,12 @@
 import { cp, readdir } from "fs/promises";
 
 const files = await readdir("./slides/dist");
+
 for (const file of files) {
   console.log(`Processing: ${file}`);
   await cp(
     `./slides/dist/${file}/assets`,
-    "./dist/assets",
+    "./slides/dist/formatted/slides/assets",
     {
       recursive: true,
     },
@@ -14,23 +15,18 @@ for (const file of files) {
 
   await cp(
     `./slides/dist/${file}/index.html`,
-    `./dist/slides/${file}.html`,
+    `./slides/dist/formatted/slides/${file}.html`,
     {
       recursive: true,
     },
     (err) => {}
   );
 }
+
 console.log(`Completing`);
 await cp(
-  `./dist/assets`,
-  `../../.vercel/output/static/assets`,
-  { recursive: true },
-  (err) => {}
-);
-await cp(
-  `./dist/slides`,
-  `../../.vercel/output/static/slides`,
-  { recursive: true },
+  `./slides/dist/formatted/slides`,
+  `../../.vercel/output/static/assets/slides`,
+  { recursive: true, force: false },
   (err) => {}
 );
