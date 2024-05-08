0baf158b1c6e71fa356ae86b9c780b2908bfecd6
samternent <sam.ternent@teamwork.com>
2024-05-08 01:40:42 +0100

    docs(changeset): Tweak async logic

---
+++ .changeset/gentle-lemons-hammer.md
@@ -0,0 +1,5 @@
+---
+"ternentdotdev": patch
+---
+
+Tweak async logic
--- apps/ternentdotdev/slides/build/format-slides.mjs
+++ apps/ternentdotdev/slides/build/format-slides.mjs
@@ -1,31 +1,30 @@
-import { cp, readdir } from "fs";
+import { cp, readdir } from "fs/promises";
 
-readdir("./slides/dist", async (err, files) => {
-  for (const file of files) {
-    console.log(`Processing: ${file}`);
-    await cp(
-      `./slides/dist/${file}/assets`,
-      "./dist/assets",
-      {
-        recursive: true,
-      },
-      (err) => {}
-    );
-
-    await cp(
-      `./slides/dist/${file}/index.html`,
-      `./dist/slides/${file}.html`,
-      {
-        recursive: true,
-      },
-      (err) => {}
-    );
-  }
-
+const files = await readdir("./slides/dist");
+for (const file of files) {
+  console.log(`Processing: ${file}`);
   await cp(
-    `./dist`,
-    `../../.vercel/output/static`,
-    { recursive: true },
+    `./slides/dist/${file}/assets`,
+    "./dist/assets",
+    {
+      recursive: true,
+    },
     (err) => {}
   );
-});
+
+  await cp(
+    `./slides/dist/${file}/index.html`,
+    `./dist/slides/${file}.html`,
+    {
+      recursive: true,
+    },
+    (err) => {}
+  );
+}
+console.log(`Completing`);
+await cp(
+  `./dist`,
+  `../../.vercel/output/static`,
+  { recursive: true },
+  (err) => {}
+);
